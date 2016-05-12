e9a484db18c050d1638a701980d24ab884dd8594
郑旭 <zhengxu@4399inc.com>
2016-05-12 14:30:17 +0800

    UPDATE INFO

--- Classes/MTDMacro.h
+++ Classes/MTDMacro.h
@@ -1,14 +1,14 @@
 //
 //  MTDMacro.h
-//  m4399_GameVideo
 //
-//  Created by 郑旭 on 15/3/10.
+//  Created by fmricky on 2016-04-18.
 //  Copyright (c) 2015年 4399 Network Co., Ltd. All rights reserved.
 //
 
 #ifndef m4399_GameVideo_MTDMacro_h
 #define m4399_GameVideo_MTDMacro_h
 
+#pragma mark - Assert
 
 static inline BOOL IsEmpty(id thing) {
     return thing == nil || [thing isEqual:[NSNull null]]
@@ -22,15 +22,12 @@ static inline BOOL IsNotEmpty(id thing) {
     return !IsEmpty(thing);
 }
 
-
-#pragma mark -
-#pragma mark Blocks
+#pragma mark - Blocks
 
 #define BLOCK_SAFE_RUN(block, ...) block ? block(__VA_ARGS__) : nil
 
 
-#pragma mark -
-#pragma mark iOS Version
+#pragma mark - iOS SYSTEM
 
 #define IOS_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define IOS_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
@@ -39,24 +36,82 @@ static inline BOOL IsNotEmpty(id thing) {
 #define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 #define IOS_VERSION                              [[UIDevice currentDevice] systemVersion]
 
+//获取当前语言
+#define CurrentLanguage ([[NSLocale preferredLanguages] objectAtIndex:0])
 
-#pragma mark -
-#pragma mark APP Info
+
+
+
+#pragma mark - APP Info
 
 #define APP_VERSION                 [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kCFBundleVersionKey]
 #define APP_NAME                    [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kCFBundleNameKey]
 
-#pragma mark -
-#pragma mark Color
 
-#define SETTINGS_TEXT_COLOR RGBCOLOR(57, 85, 135)
+
+
+#pragma mark - Color
+
 #ifndef RGBCOLOR
 #define RGBCOLOR(r, g, b) [UIColor colorWithRed : (r) / 255.0 green : (g) / 255.0 blue : (b) / 255.0 alpha : 1]
 #endif
 #define XHEXCOLOR(c) [UIColor colorWithRed : ((c >> 16) & 0xFF) / 255.0 green : ((c >> 8) & 0xFF) / 255.0 blue : (c & 0xFF) / 255.0 alpha : 1.0]
 #define XHEXCOLOR_ALPHA(c, a) [UIColor colorWithRed : ((c >> 16) & 0xFF) / 255.0 green : ((c >> 8) & 0xFF) / 255.0 blue : (c & 0xFF) / 255.0 alpha : a]
 
+//清除背景色(透明)
+#define CLEARCOLOR [UIColor clearColor]
 
+
+#pragma mark - Image
+//读取本地图片
+#define LOADIMAGE(file,ext) [UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:file ofType:ext]]
+//定义UIImage对象
+#define IMAGE(A) [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:A ofType:nil]]
+//定义UIImage对象
+#define ImageNamed(_pointer) [UIImage imageNamed:[UIUtil imageName:_pointer]]
+
+
+#pragma mark - GCD
+#define BACK(block) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+#define MAIN(block) dispatch_async(dispatch_get_main_queue(),block)
+
+#pragma mark - SINGLETON INSTANCE
+//单例化一个类
+#define SINGLETON_FOR_CLASS(classname) \
+\
+static classname *shared##classname = nil; \
+\
++ (classname *)shared##classname \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [[self alloc] init]; \
+} \
+} \
+\
+return shared##classname; \
+} \
+\
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+@synchronized(self) \
+{ \
+if (shared##classname == nil) \
+{ \
+shared##classname = [super allocWithZone:zone]; \
+return shared##classname; \
+} \
+} \
+\
+return nil; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return self; \
+}
 
 
 #endif
